ac5cb3cb1d0507b612a209481cd6c6e27ae70b24
Ivan Repeta <ivan.repeta.work@gmail.com>
2021-04-02 16:59:49 +0300

    Initial commit

---
+++ src/main/java/com/repeta/mo/App.java
@@ -0,0 +1,13 @@
+package com.repeta.mo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
