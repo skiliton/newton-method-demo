d220d846ca7306a849db8412b8ef03af5e488c8f
Ivan Repeta <ivan.repeta.work@gmail.com>
2021-04-03 13:10:42 +0300

    Add alpha division strategy for computing alpha

--- src/main/java/com/repeta/mo/App.java
+++ src/main/java/com/repeta/mo/App.java
@@ -13,14 +13,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        AlphaCalculationStrategy alphaCS = new ConstantAlpha(1);
-        MinimizationAlgorithm algorithm  = new NewtonMethod(0.001,alphaCS,300);
+
         Function<SimpleMatrix,Double> f = X -> {
             double x = X.get(0);
             double y = X.get(1);
             return 15*x*x + 18*y*y - 0.03*x*y + x - y;
         };
+        NewtonMethod algorithm  = new NewtonMethod(0.001,null,300);
         SimpleMatrix initX = new SimpleMatrix(new double[][]{{10},{10}});
+
+        algorithm.setAlphaCS(new ConstantAlpha(1));
+        algorithm.minimize(f,initX).print();
+
+        algorithm.setAlphaCS(new FirstMethodAlpha(0.001));
         algorithm.minimize(f,initX).print();
     }
 }
---
+++ src/main/java/com/repeta/mo/alpha/FirstMethodAlpha.java
@@ -0,0 +1,34 @@
+package com.repeta.mo.alpha;
+
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.function.Function;
+
+public class FirstMethodAlpha implements AlphaCalculationStrategy{
+
+    private double eps;
+
+    public FirstMethodAlpha(double eps) {
+        this.eps = eps;
+    }
+
+    public double getEps() {
+        return eps;
+    }
+
+    public void setEps(double eps) {
+        this.eps = eps;
+    }
+
+    @Override
+    public double calcAlpha(Function<SimpleMatrix, Double> f, SimpleMatrix Xk, SimpleMatrix Hk, SimpleMatrix df, SimpleMatrix d2f) {
+        double alpha = 2;
+        double fXk = f.apply(Xk);
+        double fX;
+        do{
+            alpha=alpha/2;
+            fX = f.apply(Xk.plus(Hk.scale(alpha)));
+        }while (fX - fXk > eps*alpha*df.dot(Hk));
+        return alpha;
+    }
+}
